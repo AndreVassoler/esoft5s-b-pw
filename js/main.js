3512dfa9b99a66385b2fe3deeee8525bf02348de
André <111258213+AndreVassoler@users.noreply.github.com>
2024-04-12 11:28:38 -0300

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const evolucao = urlParams.get("evolucao");
@@ -24,6 +25,42 @@ if (evolucao) {
     });
 
   h2.textContent = `Informações sobre ${evolucao}`;
+}
+
+*/
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const evolucao = urlParams.get("evolucao");
+
+if (evolucao) {
+  document.title = `Página do ${evolucao}`;
+
+  const h2 = document.querySelector(`.info-pokemon`);
+  h2.textContent = `Informações sobre ${evolucao}`;
+
+  const div = document.querySelector(`.img-pokemon`);
+  const nameToLowerCase = evolucao.toLowerCase();
+  const image = fetch(`https://pokeapi.co/api/v2/pokemon/${nameToLowerCase}`);
+
+  image
+    .then((response) => response.json())
+    .then((data) => {
+      const sprites = Object.values(data.sprites);
+      const images = sprites.filter((sprite) => typeof sprite === "string");
+      const img = document.createElement("img");
+      img.src = images[0];
+      div.appendChild(img);
+
+      let index = 0;
+      img.addEventListener("click", () => {
+        index++;
+        if (index === images.length) {
+          index = 0;
+        }
+        img.src = images[index];
+      });
+    });
 }
 
 // Função para obter a data atual 
